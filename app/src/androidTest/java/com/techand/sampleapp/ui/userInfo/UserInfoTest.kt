08e6f047a50d376e5491795c672a6b7b1bc01042
patelgayatri <gayatripatel566@gmail.com>
2021-01-12 21:00:08 +1100

    - launch fragment test implemented

--- app/src/androidTest/java/com/techand/sampleapp/ui/userInfo/UserInfoTest.kt
+++ app/src/androidTest/java/com/techand/sampleapp/ui/userInfo/UserInfoTest.kt
@@ -1,10 +1,8 @@
 package com.techand.sampleapp.ui.userInfo
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import com.techand.sampleapp.launchFragmentInHiltContainer
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
